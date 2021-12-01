76303c377dad55cc6123b494293cd7a64e5055c5
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2021-12-01 15:25:41 +0530

    Recursion 2

---
+++ Recursion 2/RemoveDuplicatesRecursively.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+
+using namespace std;
+
+void removeConsecutiveDuplicates(char *input) {
+	
+    if (input[0] == '\0')
+    {
+        return;
+    }
+    
+    if (input[0] == input[1])
+    {
+        int i = 1;
+
+        for (; i < input[i] != '\0' ; i++)
+        {
+            input[i] = input[i + 1];
+        }
+        
+        input[i] = input[i + 1];
+        removeConsecutiveDuplicates(input);
+    }
+    
+    removeConsecutiveDuplicates(input + 1);
+
+}
+
+int main() {
+    char s[100000];
+    cin >> s;
+    removeConsecutiveDuplicates(s);
+    cout << s << endl;
+}
