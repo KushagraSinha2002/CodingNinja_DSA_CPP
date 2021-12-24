926d8aab88741b5c5205b8b4104168d8c5203c11
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2021-12-24 11:13:48 +0530

    Recursion 2

--- Recursion 2/ReturnKeypadCode.cpp
+++ Recursion 2/ReturnKeypadCode.cpp
@@ -1,11 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 // Version 1 :-
-
-
-
-// #include <iostream>
-// #include <string>
-
-// using namespace std;
 
 // string dialer[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
@@ -53,44 +51,78 @@
 
 // }
 
-
-// int main(){
-//     int num;
-//     cin >> num;
-
-//     string output[10000];
-//     int count = keypad(num, output);
-//     for(int i = 0; i < count && i < 10000; i++){
-//         cout << output[i] << endl;
-//     }
-//     return 0;
-// }
-
-
+// --------------------------------------------------------------------------------
 
 // Version 2 :-
 
+// string key[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+// 			//0 , 1,  2  ,3    ,4    , 5   ,6    ,7     ,8    ,9 
+// int i=0;
+// void keypad(int num,string o,string output[])
+// {
+//     if(num==0)
+//     { 
+//         output[i++]=o;
+//         return ;
+//     }
+//     string temp=key[num%10];  // taking string corresponding to last digit
+//     for(int i=0;i<temp.size();i++)
+//         keypad(num/10,temp[i]+o,output);    //  taking every ch of temp in output 
+// }
+// int keypad(int num, string output[])
+// {
+//     string o="";
+//     keypad(num,o,output);
+//     return 100000;
+// }
 
+// --------------------------------------------------------------------------------
 
-#include<string>
-using namespace std;
+// version 3 
+
 string key[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-			//0 , 1,  2  ,3    ,4    , 5   ,6    ,7     ,8    ,9 
-int i=0;
-void keypad(int num,string o,string output[])
-{
-    if(num==0)
-    { 
-        output[i++]=o;
-        return ;
+
+int keypad(int num, string output[]){
+    if(num == 0){
+        output[0] = "";
+        return 1;
     }
-    string temp=key[num%10];  // taking string corresponding to last digit
-    for(int i=0;i<temp.size();i++)
-        keypad(num/10,temp[i]+o,output);    //  taking every ch of temp in output 
+    
+    int smallNum = num / 10;
+    int lastDigit = num % 10;
+    
+    string smallString = key[lastDigit];
+    int smallStringSize = smallString.size();
+    
+    int smallKeypadSize = keypad(smallNum, output);
+    
+    for(int i = 0; i < smallStringSize - 1; i++){
+        for(int j = 0; j < smallKeypadSize; j++){
+            output[j + (i + 1)*smallKeypadSize] = output[j];
+        }    
+    }
+    
+    int k = 0;
+    
+    for(int i = 0; i < smallStringSize; i++){
+        for(int j = 0; j < smallKeypadSize; j++){
+            output[k] = output[k] + smallString[i];
+            k++;
+        }
+    }
+    
+    return smallKeypadSize * smallStringSize;
 }
-int keypad(int num, string output[])
-{
-    string o="";
-    keypad(num,o,output);
-    return 100000;
+
+
+int main(){
+    int num;
+    cin >> num;
+
+    string output[10000];
+    int count = keypad(num, output);
+    for(int i = 0; i < count && i < 10000; i++){
+        cout << output[i] << endl;
+    }
+    return 0;
 }
