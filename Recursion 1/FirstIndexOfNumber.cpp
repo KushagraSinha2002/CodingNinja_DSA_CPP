30b8fc21a671d28c040b7abba49979d7f33ca02a
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-01-06 14:09:47 +0530

    Recursion 1

--- Recursion 1/FirstIndexOfNumber.cpp
+++ Recursion 1/FirstIndexOfNumber.cpp
@@ -2,22 +2,22 @@
 
 using namespace std;
 
-int firstIndex(int input[], int size, int x) {
-  
-    if (size == 0)
-    {
-       return -1;
+int firstIndex(int input[], int size, int x){
+    if(size == 0){
+        return -1;
     }
-    if (input[0] == x)
-    {
-      return 0;
+    int smallOutput = firstIndex(input+1,size-1,x);
+    if(input[0] == x){
+        return 0;
     }
-  
-    int smallOutput = firstIndex(input + 1, size - 1, x);
-    if(smallOutput == -1) return -1;
-    return smallOutput + 1;
-
+    if(smallOutput != -1){
+        return smallOutput + 1;
+    }
+    else{
+        return -1;
+    }
 }
+
 
 
 int main(){
