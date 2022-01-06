c403f1114e5533250063a7c1b89b9e6039802855
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-01-06 12:15:14 +0530

    Recursion 1

--- Recursion 1/SumOfDigits(recursive).cpp
+++ Recursion 1/SumOfDigits(recursive).cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-int sumOfDigits(int n){
-    
-    if(n == 0){
-        return 0;
+int sumOfDigits(int n) {
+	if(n/10 == 0){
+        return n;
     }
     
-    return n%10 + sumOfDigits(n/10);
+    int smallOutput = sumOfDigits(n/10);
     
+    return (smallOutput + (n%10));
 }
 
 int main() {
