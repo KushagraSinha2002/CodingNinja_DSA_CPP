d15b773eac15385890f0c099b2ca6ae4f2d93817
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-02-05 16:48:41 +0530

    Linked List 2

--- Linked List 2/MergeTwoSortedLL.cpp
+++ Linked List 2/MergeTwoSortedLL.cpp
@@ -6,34 +6,30 @@ Node *mergeTwoSortedLinkedLists(Node *head1, Node *head2)
     else if(head2 == NULL){
         return head1;
     }
-    Node *tempH = NULL, *tempT = NULL, *temp1 = NULL, *temp2 = NULL, *store = NULL;
+    Node *tempH, *tempT, *temp1=head1, *temp2=head2;
     if(head1->data <= head2->data){
-        
         tempH = head1;
         tempT = head1;
         temp1 = head1->next;
-        temp2 = head2;
     }
-    else {
+    else if(head2->data < head1->data){
         tempH = head2;
         tempT = head2;
-        temp1 = head1;
         temp2 = head2->next;
     }
     
-    while(temp1 != NULL && temp2 != NULL){
-    	if(temp1->data <= temp2->data){
+	while(temp1 != NULL && temp2 !=NULL){
+        if(temp1->data <= temp2->data){
             tempT->next = temp1;
-            tempT = tempT->next;
+            tempT = temp1;
             temp1 = temp1->next;
         }
         else if(temp2->data < temp1->data){
             tempT->next = temp2;
-            tempT = tempT->next;
+            tempT = temp2;
             temp2 = temp2->next;
         }
     }
-    
     if(temp1 != NULL){
         tempT->next = temp1;
     }
