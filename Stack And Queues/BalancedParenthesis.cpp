c78f0012da43d10c238b02f12f6c854ea8be431e
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-07-02 15:38:24 +0530

    Balanced Parenthesis

---
+++ Stack And Queues/BalancedParenthesis.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include <stack>
+bool isBalanced(string expression) 
+{
+    stack<char> stk;
+    for (int i = 0; i < expression.length(); i++) 
+    {
+        if (expression[i] == '(') 
+        {
+            stk.push(expression[i]);
+        } 
+        else if (expression[i] == ')') 
+        {
+            if (stk.empty()) 
+            {
+                return false;
+            }
+            char topChar = stk.top();
+            stk.pop();
+            if (expression[i] == ')' && topChar == '(') 
+            {
+                continue;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    return stk.empty();
+}
+
+int main() 
+{
+    string input;
+    cin >> input;
+    cout << ((isBalanced(input)) ? "true" : "false");
+}
