4cec0e38479d35ba8f08f94774b7af267025b588
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-07-06 12:43:48 +0530

    Node with maximum child sum

--- Trees/NodeWithMaximumChildSum.cpp
+++ Trees/NodeWithMaximumChildSum.cpp
@@ -1,3 +1,50 @@
+/*
+Node with maximum child sum
+
+Given a generic tree, find and return the node for which sum of its data and data of all its
+child nodes is maximum. In the sum, data of the node and data of its immediate child nodes
+has to be taken.
+
+Input format :
+The first line of input contains data of the nodes of the tree in level order form.
+The order is: data for root node, number of children to root node, data of each of
+child nodes and so on and so forth for each node. The data of the nodes of the tree is
+separated by space. 
+
+Output format :
+The first and only line of output contains the data of the node with maximum sum, as described
+in the task.
+
+Constraints:
+
+Time Limit: 1 sec
+
+Sample Input 1 :
+5 3 1 2 3 1 15 2 4 5 1 6 0 0 0 0
+
+Sample Output 1 :
+1
+
+Sample Input 2 :
+5 0
+
+Sample Output 2 :
+5
+
+Sample Input 3 :
+1 1 2 1 3 1 4 1 5 0
+
+Sample Output 3 :
+4
+
+Sample Input 4 :
+6 3 4 10 9 2 3 2 1 1 3 12 7 5 0 0 2 11 8 0 0 0 0 0
+
+Sample Output 4 :
+9
+*/
+
+
 #include <iostream>
 #include <queue>
 #include <vector>
