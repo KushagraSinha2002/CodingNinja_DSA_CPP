b9ca3d08b74e9f67d5880344708df62c53852d70
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-08-14 17:06:46 +0530

    Sum of Array

--- Recursion 1/SumOfArray.cpp
+++ Recursion 1/SumOfArray.cpp
@@ -1,19 +1,35 @@
-#include<iostream>
+/*
+Sum of Array
 
+Given an array of length N, you need to find and return the sum of all elements of the array.
+Do this recursively.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Output Format :
+Sum
+Constraints :
+1 <= N <= 10^3
+Sample Input 1 :
+3
+9 8 9
+Sample Output 1 :
+26
+Sample Input 2 :
+3
+4 2 1
+Sample Output 2 :
+7    
+*/
+#include<iostream>
 using namespace std;
 
 int sum(int input[], int n) {
-
-  if (n == 1)
-  {
-      return input[0];
-  }
-  
-  int smalloutput = sum(input + 1,n-1);
-  return input[0] + smalloutput;
-
+    if(n==0){
+        return input[n-1];
+	}
+    return input[n-1]+sum(input,n-1);
 }
-
 
 int main(){
     int n;
