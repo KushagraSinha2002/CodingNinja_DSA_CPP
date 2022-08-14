ceb4a91ee3c9e1352b6accf322ee3900523dcc7e
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-08-14 17:02:36 +0530

    Number of Digits

--- Recursion 1/NumberOfDigits.cpp
+++ Recursion 1/NumberOfDigits.cpp
@@ -1,17 +1,30 @@
-#include<iostream>
+/*
+Number of Digits
 
+Given the number 'n', find out and return the number of digits present in a number recursively.
+Input Format :
+Integer n
+Output Format :
+Count of digits
+Constraints :
+1 <= n <= 10^6
+Sample Input 1 :
+ 156
+Sample Output 1 :
+3
+Sample Input 2 :
+ 7
+Sample Output 2 :
+1
+*/
+#include<iostream>
 using namespace std;
 
 int count(int n){
-    
-    if (n == 0)
-    {
+    if(n==0){
         return 0;
     }
-    
-    int smallOutput = count(n/10);
-    return  smallOutput + 1;
-
+    return 1+count(n/10);
 }
 
 int main(){
