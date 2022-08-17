9e1db5b735875c64b28e1a8e886dfa63573900c4
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-08-17 18:50:14 +0530

    Remove X

--- Recursion 1b/RemoveX.cpp
+++ Recursion 1b/RemoveX.cpp
@@ -1,5 +1,5 @@
 #include <iostream> 
-
+#include<cstring>
 using namespace std;
 
 void removeX(char input[]) {
@@ -7,16 +7,16 @@ void removeX(char input[]) {
         return;
     }
     if(input[0] == 'x'){
-        int i;
-        for(i = 0; input[i] != '\0'; i++){
+        int len = strlen(input);
+        for(int i=0;i<=len;i++){
             input[i] = input[i+1];
         }
-        input[i] = '\0';
-        removeX(input);
+        if(input[0] == 'x'){
+            removeX(input);
+        }
     }
-	removeX(input+1);
+    removeX(input+1);
 }
-
 
 int main() {
     char input[100];
