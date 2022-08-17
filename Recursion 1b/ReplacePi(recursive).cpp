7d48582fb69422ea8151221d9a7aae296b225e49
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-08-17 18:40:24 +0530

    Replace pi (recursive)

--- Recursion 1b/ReplacePi(recursive).cpp
+++ Recursion 1b/ReplacePi(recursive).cpp
@@ -1,23 +1,20 @@
 #include <iostream> 
-
+#include<cstring>
 using namespace std;
 
 void replacePi(char input[]) {
-    
     if(input[0] == '\0'){
         return;
     }
-    if(input[0] == 'p'){
-        if(input[1] == 'i'){
-            int size = strlen(input);
-            for(int i = size; i != 0; i--){
-                input[i+2] = input[i];
-            }
-            input[0] = '3';
-            input[1] = '.';
-            input[2] = '1';
-            input[3] = '4';
+    if(input[0] == 'p' && input[1] == 'i'){
+        int len = strlen(input);
+        for(int i=len;i!=0;i--){
+            input[i+2] = input[i];
         }
+        input[0] = '3';
+        input[1] = '.';
+        input[2] = '1';
+        input[3] = '4';
     }
     replacePi(input+1);
 }
