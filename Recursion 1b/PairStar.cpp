bed0cab275ebc8c71dedf50295999b033eb56fc5
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-08-17 19:22:48 +0530

    Pair Star

--- Recursion 1b/PairStar.cpp
+++ Recursion 1b/PairStar.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 void pairStar(char input[]) {
-	if(input[0] == '\0'){
-       return; 
+    if(input[0] == '\0'){
+        return;
     }
     if(input[0] == input[1]){
-        for(int i = strlen(input); i != 0; i--){
+        int len = strlen(input);
+        for(int i=len;i>=0;i--){
             input[i+1] = input[i];
         }
         input[1] = '*';
-        pairStar(input);
     }
     pairStar(input+1);
 }
