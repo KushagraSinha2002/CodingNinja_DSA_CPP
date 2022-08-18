168fe4bc59f0af8065101692505ac71f18067d86
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-08-18 10:31:12 +0530

    Remove Duplicates Recursively

--- Recursion 2/RemoveDuplicatesRecursively.cpp
+++ Recursion 2/RemoveDuplicatesRecursively.cpp
@@ -1,31 +1,19 @@
 #include <iostream>
-
 using namespace std;
 
 void removeConsecutiveDuplicates(char *input) {
-	/* Don't write main().
-	* Don't read input, it is passed as function argument.    
-	* Change in the given string itself.
-	* No need to return or print anything
-	* Taking input and printing output is handled automatically.
-	*/
-    
     if(input[0] == '\0'){
         return;
     }
-    
     if(input[0] == input[1]){
-        int i = 0;
-        for(i; input[i] != '\0'; i++){
+        for(int i=0;input[i]!='\0';i++){
             input[i] = input[i+1];
         }
-        input[i] = '\0';
-        removeConsecutiveDuplicates(input);
+        if(input[0] == input[1]){
+            removeConsecutiveDuplicates(input);
+        }
     }
-    
-    removeConsecutiveDuplicates(input + 1);
-
-
+    removeConsecutiveDuplicates(input+1);
 }
 
 int main() {
